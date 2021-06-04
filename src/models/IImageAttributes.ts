520228149240ccd641d537e7b698f359e33d5d8b
Tóth Márk Zoltán <zollee60@gmail.com>
2021-06-04 08:50:10 +0200

    Model interface

---
+++ src/models/IImageAttributes.ts
@@ -0,0 +1,11 @@
+export interface IImageAttributes {
+    id?: number
+    name?: string,
+    originalName: string,
+    url?: string,
+    source: string,
+    size?: number,
+    dimensions?: string,
+    description: string,
+    createdAt: Date
+}
