afab15c1e51635bbd98485814aff9e05495b46c6
Tóth Márk Zoltán <zollee60@gmail.com>
2021-06-04 08:52:24 +0200

    App component / assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import lime from './assets/lime.svg';
 import './App.css';
+import ImageProvider from './components/ImageProvider';
+import ImageSet from './components/ImageSet';
+import ReactModal from 'react-modal';
 
-function App() {
+ReactModal.setAppElement('#root');
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ImageProvider>
+      <div className="w-screen h-screen">
+        <div className="bg-lime md:h-24 shadow-2xl items-center flex">
+          <div className="w-max flex ml-10">
+            <img src={lime} alt="lime" className="w-12 mr-3" />
+            <h1 className="text-4xl font-bold text-gray-700 mt-1">LimeCRM</h1>
+          </div>
+        </div>
+        <ImageSet />
+      </div>
+    </ImageProvider>
   );
 }
 
